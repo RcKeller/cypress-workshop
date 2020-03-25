f55a9c42741be880af88e47265473232afc9cdc5
Ryan Keller <rykeller.tech@gmail.com>
2020-03-24 21:06:55 -0700

    Simple example

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
+// import React from 'react'
+// import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
 
-export default () => <Title>My page</Title>
+// export default () => <Title>My page</Title>
+
+import fetch from 'isomorphic-unfetch'
+import Link from 'next/link'
+
+const Index = ({ users }) => (
+  <ul>
+    {users.map(user => (
+      <li key={user.id}>
+        <Link href='/user/[id]' as={`/user/${user.id}`}>
+          <a>{`User ${user.id}`}</a>
+        </Link>
+      </li>
+    ))}
+  </ul>
+)
+
+Index.getInitialProps = async () => {
+  const response = await fetch('http://localhost:3000/api/users')
+  const users = await response.json()
+
+  return { users }
+}
+
+export default Index
